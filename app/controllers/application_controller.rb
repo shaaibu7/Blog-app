3d26cc7740eda70ce31386f830c0bd8cf34b5b89
shaaibu <shaaibusuleiman@gamil.com>
2023-05-31 10:57:03 +0100

    Implement functionality for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
-  before_action :authenticate_user!
 
   protected
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ActionController::Base
-  before_action :authenticate_user!
 
   def index
     @users = User.all
   end
 
   def show
-    if (params[:id] = 'sign out')
+    if params[:id] == 'sign_out'
       sign_out_and_redirect
     else
       find_user
