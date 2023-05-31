3aba9d985bc7bbcf105e99fbfe9d4b81761566a0
shaaibu <shaaibusuleiman@gamil.com>
2023-05-31 08:19:57 +0100

    Implement login and sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  attribute :posts_counter, :integer, default: 0
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
