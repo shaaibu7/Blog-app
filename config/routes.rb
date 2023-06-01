872bb690c0ac2e7cb3d22586003d11c4ef822447
shaaibu <shaaibusuleiman@gamil.com>
2023-06-01 10:33:27 +0100

    Add role column to users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: 'confirmations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -11,6 +13,15 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
+    end
+  end
+
+  devise_scope :user do
+    authenticated :user do
+      root 'users#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'users#index', as: :unauthenticated_root
     end
   end
 end
